929bbf6de10d277a386ff9de0f93bb38b20f3e45
jtabach <jeff.n.tabachnick@gmail.com>
2018-08-04 00:24:22 +0200

    fetch demo on interval for mock service worker

--- src/components/DemoWorker.js
+++ src/components/DemoWorker.js
@@ -4,24 +4,22 @@ import axios from 'axios';
 import { connect } from 'react-redux';
 import { fetchWorkerDemo } from '../actions/demo';
 
+const intervalFrequency = 3000;
+
 class DemoWorker extends Component {
   componentDidMount() {
     this.props.fetchWorkerDemo();
+    setInterval(() => {
+      this.props.fetchWorkerDemo();
+      console.log('fetching');
+    }, intervalFrequency);
   }
-
-  // _handleClick() {
-  //   console.log('click');
-  //   axios.get('http://localhost:5000').then(res => {
-  //     console.log(res);
-  //   });
-  // }
 
   render() {
     console.log(this.props.demo);
     return (
       <div>
         <div>Service Worker Test</div>
-        {/* <button onClick={this._handleClick.bind(this)}>Fetch Data</button> */}
       </div>
     );
   }
@@ -34,4 +32,3 @@ function mapStateToProps({ demo }) {
 }
 
 export default connect(mapStateToProps, { fetchWorkerDemo })(DemoWorker);
-// export default DemoWorker;
--- src/reducers/demo.js
+++ src/reducers/demo.js
@@ -1,7 +1,6 @@
 import { FETCH_WORKER_DEMO_COMPLETED } from '../types/demo';
 
 export default (state = null, action) => {
-  console.log(action);
   switch (action.type) {
     case FETCH_WORKER_DEMO_COMPLETED:
       return action.payload.data;
