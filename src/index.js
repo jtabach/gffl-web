775db208e87442e098f6061dfdd0ffe6676b727b
jtabach <jeff.n.tabachnick@gmail.com>
2018-08-03 15:05:39 +0200

    setup actions, reducers, types w sample message

--- src/index.js
+++ src/index.js
@@ -5,8 +5,9 @@ import { createStore, applyMiddleware } from 'redux';
 import reduxThunk from 'redux-thunk';
 
 import App from './components/App';
+import reducers from './reducers';
 
-const store = createStore(() => {}, {}, applyMiddleware());
+const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
   <Provider store={store}>
