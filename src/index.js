5e41b15bc4fa734fe003fa7dd640818809380bc7
jtabach <jeff.n.tabachnick@gmail.com>
2018-08-06 12:11:25 +0200

    feat(login): renders login form with redux form

--- src/index.js
+++ src/index.js
@@ -9,7 +9,11 @@ import routes from './routes';
 import App from './components/App';
 import reducers from './reducers';
 
-const store = createStore(reducers, applyMiddleware(thunk));
+const store = createStore(
+  reducers,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(thunk)
+);
 
 ReactDOM.render(
   <Provider store={store}>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
+import { reducer as formReducer } from 'redux-form';
 
 import demoReducer from './demo';
 import authReducer from './auth';
 
 export default combineReducers({
   demo: demoReducer,
-  isLoggedIn: authReducer
+  isLoggedIn: authReducer,
+  form: formReducer
 });
