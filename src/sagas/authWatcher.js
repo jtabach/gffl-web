0d664c18c974c572bdb5e1d79bde81f0c1cabfb1
jtabach <jeff.n.tabachnick@gmail.com>
2018-08-28 14:15:30 +0200

    combine auth watchers + use types

--- src/sagas/authWatcher.js
+++ src/sagas/authWatcher.js
@@ -1,5 +1,11 @@
 import { put, takeLatest, all, call } from 'redux-saga/effects';
-import { postRequest } from './helpers/request';
+import { getRequest, postRequest } from './helpers/request';
+import {
+  LOGIN_USER,
+  LOGIN_USER_COMPLETED,
+  FETCH_USER,
+  FETCH_USER_COMPLETED
+} from '../types/auth';
 
 function* loginUserRequest(action) {
   const response = yield call(
@@ -8,13 +14,22 @@ function* loginUserRequest(action) {
     action.payload
   );
   if (response.user) {
-    yield put({ type: 'LOGIN_USER_COMPLETED', payload: { data: response } });
+    yield put({ type: LOGIN_USER_COMPLETED, payload: { data: response } });
   } else {
     // invoke some other action
     console.log('handle failed login');
   }
 }
 
+function* fetchUserRequest(action) {
+  const response = yield call(
+    getRequest,
+    'http://localhost:5000/api/auth/user'
+  );
+  yield put({ type: FETCH_USER_COMPLETED, payload: { data: response } });
+}
+
 export function* authWatcher() {
-  yield takeLatest('LOGIN_USER', loginUserRequest);
+  yield takeLatest(LOGIN_USER, loginUserRequest);
+  yield takeLatest(FETCH_USER, fetchUserRequest);
 }
--- src/sagas/index.js
+++ src/sagas/index.js
@@ -1,7 +1,6 @@
 import { all } from 'redux-saga/effects';
 import { authWatcher } from './authWatcher';
-import { userWatcher } from './user';
 
 export default function* rootSaga() {
-  yield all([authWatcher(), userWatcher()]);
+  yield all([authWatcher()]);
 }
