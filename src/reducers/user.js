10b220f15718784a832294e40a01cab28fae7cc4
jtabach <jeff.n.tabachnick@gmail.com>
2018-08-29 00:45:31 +0200

    reducer for join league completed

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -5,7 +5,10 @@ import {
   FETCH_USER_COMPLETED
 } from '../types/auth';
 
-import { CREATE_LEAGUE_COMPLETED } from '../types/league';
+import {
+  CREATE_LEAGUE_COMPLETED,
+  JOIN_LEAGUE_COMPLETED
+} from '../types/league';
 
 export default (state = null, action) => {
   console.log(action);
@@ -17,6 +20,8 @@ export default (state = null, action) => {
     case FETCH_USER_COMPLETED:
       return action.payload.data.user;
     case CREATE_LEAGUE_COMPLETED:
+      return action.payload.data.user;
+    case JOIN_LEAGUE_COMPLETED:
       return action.payload.data.user;
     case LOGOUT_USER_COMPLETED:
       return false;
