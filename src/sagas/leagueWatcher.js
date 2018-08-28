97e65ae41fb0cf7b9d7be94647636c169c0f823e
jtabach <jeff.n.tabachnick@gmail.com>
2018-08-28 14:59:53 +0200

    use redux saga for join league

--- src/sagas/leagueWatcher.js
+++ src/sagas/leagueWatcher.js
@@ -1,7 +1,12 @@
 import { put, takeLatest, all, call } from 'redux-saga/effects';
 import { postRequest } from './helpers/request';
 
-import { CREATE_LEAGUE, CREATE_LEAGUE_COMPLETED } from '../types/league';
+import {
+  CREATE_LEAGUE,
+  CREATE_LEAGUE_COMPLETED,
+  JOIN_LEAGUE,
+  JOIN_LEAGUE_COMPLETED
+} from '../types/league';
 
 function* createLeagueRequest(action) {
   const response = yield call(
@@ -18,6 +23,22 @@ function* createLeagueRequest(action) {
   }
 }
 
+function* joinLeagueRequest(action) {
+  const response = yield call(
+    postRequest,
+    'http://localhost:5000/api/team',
+    action.payload
+  );
+  // TODO: some conditional to show league was joined successfully
+  if (true) {
+    yield put({ type: JOIN_LEAGUE_COMPLETED, payload: { data: response } });
+  } else {
+    // invoke some other action
+    console.log('handle failed join league');
+  }
+}
+
 export function* leagueWatcher() {
   yield takeLatest(CREATE_LEAGUE, createLeagueRequest);
+  yield takeLatest(JOIN_LEAGUE, joinLeagueRequest);
 }
