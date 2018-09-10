da93ba8c847c3664cbc597c4105a8d84f109c40b
jtabach <jeff.n.tabachnick@gmail.com>
2018-09-10 16:44:11 +0200

    all proptypes for league route

--- src/components/league/index.js
+++ src/components/league/index.js
@@ -15,18 +15,35 @@ class League extends Component {
       name: PropTypes.string.isRequired,
       _id: PropTypes.string.isRequired,
       posts: PropTypes.array,
-      teams: PropTypes.array.isRequired
-    })
-  };
-
-  static defaultProps = {
-    league: {
-      admin: ''
-    }
+      teams: PropTypes.arrayOf(
+        PropTypes.shape({
+          name: PropTypes.string.isRequired,
+          league: PropTypes.string.isRequired,
+          _id: PropTypes.string.isRequired,
+          user: PropTypes.object.isRequired
+        })
+      )
+    }).isRequired,
+    user: PropTypes.shape({
+      email: PropTypes.string.isRequired,
+      _id: PropTypes.string.isRequired,
+      teams: PropTypes.arrayOf(
+        PropTypes.shape({
+          name: PropTypes.string.isRequired,
+          _id: PropTypes.string.isRequired,
+          user: PropTypes.string.isRequired,
+          league: PropTypes.object.isRequired
+        })
+      )
+    }).isRequired,
+    match: PropTypes.shape({
+      url: PropTypes.string.isRequired
+    }).isRequired
   };
 
   render() {
     const { match, league, user } = this.props;
+    console.log(user);
 
     return (
       <div>
