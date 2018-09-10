c857f96aead63fd0aae04d3b3ee63a53ac9750e0
jtabach <jeff.n.tabachnick@gmail.com>
2018-09-10 20:27:43 +0200

    PropsRoute component for passing props in a route

--- src/components/league/LeagueRoutes.js
+++ src/components/league/LeagueRoutes.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link, Route, Switch } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
+import PropsRoute from '../PropsRoute';
 import Timeline from './Timeline';
 import Members from './Members';
 import MyTeam from './MyTeam';
@@ -20,7 +21,7 @@ class LeagueRoutes extends Component {
     return (
       <Switch>
         <Route exact path={`${match.url}`} component={Timeline} />
-        <Route exact path={`${match.url}/members`} component={Members} />
+        <PropsRoute exact path={`${match.url}/members`} component={Members} />
         <Route exact path={`${match.url}/myteam`} component={MyTeam} />
         <Route exact path={`${match.url}/leagueInfo`} component={LeagueInfo} />
         {/* TODO: wildcard may want to show 'error' page with redirect links */}
