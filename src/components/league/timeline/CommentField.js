251feb4292836f6f34025ba1052e0f53c930111f
jtabach <jeff.n.tabachnick@gmail.com>
2018-09-11 19:12:06 +0200

    CommentField component nested in PostItem component git push

---
+++ src/components/league/timeline/CommentField.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Textarea from 'react-textarea-autosize';
+import Button from '../../common/Button';
+
+class CommentField extends Component {
+  // static propTypes = {
+  //   onPostInputChange: PropTypes.func.isRequired,
+  //   onPostInputSubmit: PropTypes.func.isRequired,
+  //   text: PropTypes.string.isRequired
+  // };
+
+  render() {
+    const { onCommentInputChange, onCommentInputSubmit } = this.props;
+
+    return (
+      <div>
+        <Textarea
+          inputRef={tag => (this.textarea = tag)}
+          type="text"
+          placeholder="Say Something!"
+          value={this.props.text}
+          onChange={() => onCommentInputChange(this.textarea.value)}
+        />
+        <Button
+          type="button"
+          variant="primary"
+          label="comment"
+          onClick={() => onCommentInputSubmit(this.textarea.value)}
+        />
+      </div>
+    );
+  }
+}
+
+export default CommentField;
--- src/components/league/timeline/PostItem.js
+++ src/components/league/timeline/PostItem.js
@@ -1,11 +1,48 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import CustomPropTypes from '../../../prop-types';
 
+import Button from '../../common/Button';
+import CommentField from './CommentField';
+
 class PostItem extends Component {
   static propTypes = {
+    league: CustomPropTypes.league.isRequired, // from redux
+    team: CustomPropTypes.team.isRequired, // from redux
     post: CustomPropTypes.post.isRequired
   };
+
+  state = {
+    commentText: ''
+  };
+
+  constructor(props) {
+    super(props);
+    this.handleCommentInputChange = this.handleCommentInputChange.bind(this);
+    this.handleCommentInputSubmit = this.handleCommentInputSubmit.bind(this);
+  }
+
+  handleCommentInputChange(text) {
+    this.setState({
+      commentText: text
+    });
+  }
+
+  handleCommentInputSubmit(text) {
+    const { league, team, post } = this.props;
+    const commentData = {
+      text,
+      leagueId: league._id,
+      teamId: team._id,
+      postId: post._id
+    };
+    // createComment(commentData);
+    // TODO: should only reset on successful comment
+    this.setState({
+      commentText: ''
+    });
+  }
 
   render() {
     const { post } = this.props;
@@ -14,9 +51,24 @@ class PostItem extends Component {
       <li>
         <h5>{post.team.name}</h5>
         <p>{post.text}</p>
+        <CommentField
+          onCommentInputChange={this.handleCommentInputChange}
+          onCommentInputSubmit={this.handleCommentInputSubmit}
+          text={this.state.commentText}
+        />
+        {/* <Button
+          type="button"
+          variant="primary"
+          label="like"
+          onClick={() => console.log('like')}
+        /> */}
       </li>
     );
   }
 }
 
-export default PostItem;
+function mapStateToProps({ league, team }) {
+  return { league, team };
+}
+
+export default connect(mapStateToProps)(PostItem);
