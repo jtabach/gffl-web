50da59aad2f844f49069818005d6b614b7327dd8
jtabach <jeff.n.tabachnick@gmail.com>
2018-09-19 13:48:39 +0200

    post actions map over children + proptypes

--- src/components/league/timeline/PostActionsList.js
+++ src/components/league/timeline/PostActionsList.js
@@ -5,12 +5,31 @@ import onClickOutside from 'react-onclickoutside';
 import styles from './PostActionsList.scss';
 
 class PostActions extends Component {
+  static propTypes = {
+    children: PropTypes.oneOfType([
+      PropTypes.arrayOf(PropTypes.node),
+      PropTypes.node
+    ]).isRequired
+  };
+
   handleClickOutside = evt => {
     this.props.onHandleClickOutside();
   };
 
   render() {
-    return <div styleName="actions-list">{this.props.children}</div>;
+    return (
+      <div styleName="actions">
+        <ul styleName="actions__list">
+          {this.props.children.map(child => {
+            return (
+              <li styleName="actions__list--item" key={Math.random()}>
+                {child}
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    );
   }
 }
 
