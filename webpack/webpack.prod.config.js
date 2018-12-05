cf19e6f889c7ef0be778a2cac2dad1ca361b723b
jtabach <jeff.n.tabachnick@gmail.com>
2018-12-05 11:31:46 +0900

    add production webpack config

---
+++ webpack/webpack.prod.config.js
@@ -0,0 +1,59 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var parentDir = path.join(__dirname, '../');
+
+module.exports = {
+  entry: [
+    '@babel/polyfill',
+    'whatwg-fetch',
+    path.join(parentDir, 'src/index.js')
+  ],
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        preLoaders: ['source-map-loader']
+      }
+    ],
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          'style-loader',
+          'css-loader?modules&importLoaders=1&localIdentName=[path]___[name]__[local]___[hash:base64:5]',
+          'sass-loader'
+        ]
+      },
+      {
+        test: /\.(png|jp(e*)g|svg)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8000, // Convert images < 8kb to base64 strings
+              name: 'images/[hash]-[name].[ext]'
+            }
+          }
+        ]
+      }
+    ]
+  },
+  output: {
+    path: parentDir + '/public',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: parentDir,
+    historyApiFallback: true
+  },
+  mode: 'production'
+};
